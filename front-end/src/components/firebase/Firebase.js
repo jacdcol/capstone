c9d1f28fb75b8757ee5e4ac88c34bd36b2ab915b
jacdcol <jakedcoll@gmail.com>
2022-07-18 15:21:18 -0500

    created react app added Firebase to react app

---
+++ front-end/src/components/firebase/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAU56vNwdYJxOJD1nswmv3sFneePYWh60",
+    authDomain: "capstone-e9137.firebaseapp.com",
+    projectId: "capstone-e9137",
+    storageBucket: "capstone-e9137.appspot.com",
+    messagingSenderId: "459664929347",
+    appId: "1:459664929347:web:88eb86176dd62d06f45af6"
+};
+
+const app = initializeApp(firebaseConfig);
